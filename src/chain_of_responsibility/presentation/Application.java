5c1debd36c4957a8a4db36a3ac4722b699f270ae
nastya-andreeva <nastya200458@gmail.com>
2023-12-25 22:21:07 +0300

    New application

--- src/chain_of_responsibility/presentation/Application.java
+++ src/chain_of_responsibility/presentation/Application.java
@@ -4,15 +4,12 @@ import chain_of_responsibility.domain.*;
 
 
 public class Application {
-
     public static void main(String[] args) {
-        AbstractChainHandler header = new HeadersChainHandler();
-        AbstractChainHandler method = new MethodChainHandler();
-        AbstractChainHandler path = new PathChainHandler();
-        AbstractChainHandler session = new SessionChainHandler();
-
-        header.next(method);
-        method.next(path);
-        path.next(session);
+        String inputStream = "method:GET\npath:/user\nsession:example";
+        ChainService service = new ChainService();
+        Request request = service.exec(inputStream);
+        System.out.println(request.getMethod());
+        System.out.println(request.getHeaders());
+        System.out.println(request.getPath());
     }
 }
